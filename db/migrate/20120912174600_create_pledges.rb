bbf7dd6b481cd0c49740a75b07cb1dbf1e2486e2
Chris Chike <cchike@seas.upenn.edu>
2012-09-13 20:15:50 -0400

    New stuff

---
+++ db/migrate/20120912174600_create_pledges.rb
@@ -0,0 +1,12 @@
+class CreatePledges < ActiveRecord::Migration
+  def change
+    create_table :pledges do |t|
+      t.string :issue_url
+      t.string :issue_title
+      t.string :string
+      t.decimal :amount
+
+      t.timestamps
+    end
+  end
+end
